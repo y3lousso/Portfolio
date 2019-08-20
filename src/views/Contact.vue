0611937e7fa444f5a20609384ea2b59639c17292
y3lousso <y3lousso@gmail.com>
2019-08-20 15:24:40 +0200

    change content to coming soon ..

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,13 +1,7 @@
 <template>
   <div>
     <h3 class="text-center">{{ $t("contact") }}</h3>
-      <q-scroll-area style="height: 200px; max-width: 300px;">
-        <div v-for="n in 100" :key="n" class="q-py-xs">
-          Lorem ipsum dolor sit amet, consectetur adipisicing
-          elit, sed do eiusmod tempor incididunt ut labore et
-          dolore magna aliqua.
-        </div>
-      </q-scroll-area>
+    <div>Content coming soon..</div>
   </div>
 </template>
 
