672d2cd417ba910ff97be4c58d701132a9e1cf7e
y3lousso <y3lousso@gmail.com>
2019-08-21 16:58:55 +0200

    added dummy content contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,48 @@
 <template>
   <div>
     <h3 class="text-center">{{ $t("contact") }}</h3>
-    <div>Content coming soon..</div>
+    <div class="q-pa-md" style="max-width: 400px">
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
+
+      <q-input
+        filled
+        v-model="name"
+        label="Your mail *"
+        hint="Mail"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
+
+      <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+   </div>
   </div>
 </template>
 
@@ -10,9 +51,41 @@
 </style>
 
 <script>
-
 export default {
   name: 'ContactPage',
-};
+  data() {
+    return {
+      name: null,
+      age: null,
 
+      accept: true,
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      if (this.accept !== true) {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'fas fa-exclamation-triangle',
+          message: 'You need to accept the license and terms first',
+        });
+      } else {
+        this.$q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'fas fa-check-circle',
+          message: 'Submitted',
+        });
+      }
+    },
+
+    onReset() {
+      this.name = null;
+      this.age = null;
+      this.accept = false;
+    },
+  },
+};
 </script>
